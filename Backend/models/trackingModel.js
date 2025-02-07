68e8c374fe8a73e4f93277f589d7aa4e510f22c9
dakshjadhav16 <156324780+dakshjadhav16@users.noreply.github.com>
2025-02-07 13:28:12 +0530

    Update trackingModel.js

--- Backend/models/trackingModel.js
+++ Backend/models/trackingModel.js
@@ -1,41 +1,40 @@
 const mongoose = require('mongoose');
 
 const trackingSchema = mongoose.Schema({
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'users',
-        required:true
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        required: true
     },
-    foodName:{
-        type:String,
-        required:true
+    foodName: {
+        type: String,
+        required: true
     },
-    foodId:{
-        type:String,
-       // ref:'foods',
-        required:true
+    foodId: {
+        type: String,
+        required: true
     },
-    details:{
-       
-        calories:Number,
-        protein:Number,
-        carbohydrates:Number,
-        fat:Number,
-        fiber:Number,
-       
+    details: {
+        calories: Number,
+        protein: Number,
+        carbohydrates: Number,
+        fat: Number,
+        fiber: Number,
     },
-    eatenDate:{
-        type:String,
-        default:new Date().toLocaleDateString()
+    eatenDate: {
+        type: String,
+        default: () => {
+            const date = new Date();
+            return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`; // Formats as DD/MM/YYYY
+        }
     },
-    quantity:{
-        type:Number,
-        min:1,
-        required:true
+    quantity: {
+        type: Number,
+        min: 1,
+        required: true
     }
-},{timestamps:true})
+}, { timestamps: true });
 
-
-const trackingModel = mongoose.model("trackings",trackingSchema);
+const trackingModel = mongoose.model("trackings", trackingSchema);
 
 module.exports = trackingModel;
